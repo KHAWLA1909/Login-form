280a96661b48f2d8d14de5b805c6d8e6e599a1e8
Khawla <khawla@email.com>
2025-10-27 15:12:29 +0100

    all files

--- login.js
+++ login.js
@@ -8,17 +8,11 @@ form.addEventListener("submit", function(e) {
 
     // نجيب القيم
     const username = document.getElementById("username").value;
-    const password = document.getElementById("lastname").value;
+    const lastname = document.getElementById("lastname").value;
 
     // نتحقق من المدخلات
-    if (username === "" || lastname === "") {
+    if (username || lastname) {
         message.textContent = "المرجو إدخال جميع الحقول!";
         message.style.color = "red";
-    } else if (username === "admin" && lastname === "hello") {
-        message.textContent = "تسجيل الدخول ناجح ✅";
-        message.style.color = "green";
-    } else {
-        message.textContent = "اسم المستخدم أو كلمة  صحيحة ❌";
-        message.style.color = "red";
     }
-});
+})
